dc34cc496e199067b52e83daed60bc2f0f3f7d9b
RafitaSoler <rafaelsolerfollana@gmail.com>
2026-06-12 18:42:38 +0200

    Project finished

---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,85 @@
+﻿namespace CalculatorLibrary
+{
+    public enum Operator
+    {
+        Addition,
+        Substraction,
+        Multiplication,
+        Division,
+        Exponentiation,
+        SquareRoot,
+        Exponent10,
+        Sin,
+        Cos,
+        Tan
+    }
+
+    public record struct Operation(Operator @operator, double number1, double number2, double result);
+
+    public class Calculator
+    {
+        List<Operation> previousOperations = new();
+        Logger logger = new();
+
+        public double DoOperation(double num1, double num2, int op)
+        {
+            double result = double.NaN;
+            Operator @operator = (Operator)op;
+            switch (@operator)
+            {
+                case Operator.Addition:
+                    result = num1 + num2;
+                    break;
+                case Operator.Substraction:
+                    result = num1 - num2;
+                    break;
+                case Operator.Multiplication:
+                    result = num1 * num2;
+                    break;
+                case Operator.Division:
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                case Operator.Exponentiation:
+                    result = Math.Pow(num1, num2);
+                    break;
+                case Operator.SquareRoot:
+                    result = Math.Sqrt(num1);
+                    break;
+                case Operator.Exponent10:
+                    result = Math.Pow(10, num1);
+                    break;
+                case Operator.Sin:
+                    result = Math.Sin(num1);
+                    break;
+                case Operator.Cos:
+                    result = Math.Cos(num1);
+                    break;
+                case Operator.Tan:
+                    result = Math.Tan(num1);
+                    break;
+                default:
+                    break;
+            }
+            previousOperations.Add(new Operation(@operator, num1, num2, result));
+            return result;
+        }
+
+        public void Finish()
+        {
+            logger.Log(previousOperations);
+        }
+
+        public List<Operation> GetOperations()
+        {
+            return previousOperations;
+        }
+
+        public void DeleteHistory()
+        {
+            previousOperations.Clear();
+        }
+    }
+}
---
+++ CalculatorLibrary/Logger.cs
@@ -0,0 +1,45 @@
+﻿using CalculatorLibrary;
+using Newtonsoft.Json;
+
+internal class Logger
+{
+    JsonWriter writer;
+
+    public void Log(List<Operation> previousOperations)
+    {
+        StreamWriter logFile = File.CreateText("calculatorlog.json");
+        logFile.AutoFlush = true;
+        writer = new JsonTextWriter(logFile);
+        writer.Formatting = Formatting.Indented;
+        writer.WriteStartObject();
+        writer.WritePropertyName("Operation count");
+        writer.WriteValue(previousOperations.Count);
+        writer.WritePropertyName("Operations");
+        writer.WriteStartArray();
+        LogOperationEntries(previousOperations);
+        writer.WriteEndArray();
+        writer.WriteEndObject();
+        writer.Close();
+    }
+
+    public void LogOperationEntries(List<Operation> previousOperations)
+    {
+        foreach (Operation operation in previousOperations)
+        {
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operation");
+            writer.WriteValue(operation.@operator.ToString());
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(operation.number1);
+            if(!double.IsNaN(operation.number2))
+            {
+                writer.WritePropertyName("Operand2");
+                writer.WriteValue(operation.number2);
+            }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(operation.result);
+            writer.WriteEndObject();
+        }
+
+    }
+}
